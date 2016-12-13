b41d1950c4b0a5aac575d70bc1046e517c7d1a7a
Stephen Fraser <stephen.fraser@digirati.co.uk>
2016-12-13 11:27:15 +0000

    ELPP-1931 Updated smoke tests

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 set -ex
-composer sync
-./bin/update
-[ $(curl --write-out %{http_code} --silent --output /dev/null $(hostname)/medium-articles) == 200 ]
+
+host=$(hostname)
+# host="localhost:8088"
+
+successResponse=$(curl --write-out %{http_code} --silent --output /dev/null ${host}/medium-articles)
+
+if [ "$successResponse" == 200 ]
+then
+    exit 1
+else
+   curl ${host}/medium-articles
+   exit 0
+fi
