48a753f900b343e3005ad6448cd416ee3ff33780
Stephen Fraser <stephen.fraser@digirati.co.uk>
2017-01-19 14:48:52 +0000

    ELPP-2091 Added limitation test that passed

--- tests/src/RSS/ArticleParserTest.php
+++ tests/src/RSS/ArticleParserTest.php
@@ -47,6 +47,10 @@ final class ArticleParserTest extends PHPUnit_Framework_TestCase
         $text = $parser->parseParagraph('<p>no class on paragraph</p>');
         $this->assertEquals('no class on paragraph', $text);
 
+        // I'm not sure why this works.
+        $text = $parser->parseParagraph('<p>no class on paragraph<p>Nested paragraph</p></p>');
+        $this->assertEquals('no class on paragraph', $text);
+
         $text = $parser->parseParagraph('<p>no class on paragraph <a href="#">with link</a></p>');
         $this->assertEquals('no class on paragraph <a href="#">with link</a>', $text);
     }
